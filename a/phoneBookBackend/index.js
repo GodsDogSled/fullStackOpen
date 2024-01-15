2ddeab89c76a4f971904019ea8ed22e59d3949b1
GodsDogSled <gabekelly@hotmail.ca>
2024-01-15 14:36:22 -0800

    line change

--- a/phoneBookBackend/index.js
+++ a/phoneBookBackend/index.js
@@ -12,9 +12,10 @@
 
 const express = require('express')
 const morgan = require('morgan')
-app.use(express.static('dist'))
+
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
 // app.use(requestLogger)
 // app.use(morgan(function (tokens, req, res) {
 //   return [
